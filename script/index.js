e85f83ac97738ca00c502f01bd8f11cdfcdd21ed
김유나 <gkftndltekdbsk@naver.com>
2023-03-13 16:08:13 +0900

    1차 작업물

---
+++ script/index.js
@@ -0,0 +1,35 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+var swiper = new Swiper(".GalaxyTabS8", {
+  spaceBetween:12,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+});
+
+var swiper = new Swiper(".GalaxyTabS", {
+  spaceBetween:12,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+});
+
+var swiper = new Swiper(".GalaxyTabA", {
+  spaceBetween:12,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+});
+
+var swiper = new Swiper(".GalaxyTabAct", {
+  spaceBetween:12,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+});
